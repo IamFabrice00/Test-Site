3a476a515d42bb47e1fc7fc4ca3fca9646473571
Logon Fabrice <fabrice.logon@example.com>
2026-05-22 13:24:57 +0200

    fix: wrap 3D scene in Suspense and whitelist Drei environment CDNs in CSP

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import { Canvas } from '@react-three/fiber'
 import Scene from './components/Scene'
 import Overlay from './components/Overlay'
 import { AdaptiveDpr, AdaptiveEvents } from '@react-three/drei'
+import { Suspense } from 'react'
 
 function App() {
   return (
     <>
       <div className="canvas-container">
         <Canvas dpr={[1, 2]} gl={{ antialias: true }}>
-          <Scene />
+          <Suspense fallback={null}>
+            <Scene />
+          </Suspense>
           <AdaptiveDpr pixelated />
           <AdaptiveEvents />
         </Canvas>
@@ -17,5 +20,6 @@ function App() {
     </>
   )
 }
+
 
 export default App
